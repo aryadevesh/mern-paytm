024cd6dbb9c2892b3768dd922d2c0dd229f44e25
Devesh Kumar Arya <aryadevesh78@gmail.com>
2024-09-02 09:58:08 +0530

    port

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mainRouter = require("./routes/index");
 const cors = require("cors");
 const app = express();
-app.use(cors());
+
+app.use(cors({
+    origin: 'https://mern-paytm-devesh.vercel.app',
+}));
 app.use(express.json());
 
 app.use("/api/v1", mainRouter);
 
-
+app.listen(3000);
